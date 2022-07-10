1a5c82f1fb55e2db5400a4169bf78ba2814f6f32
ArjunkumarRA <101347086+ArjunkumarRA@users.noreply.github.com>
2022-07-10 15:51:16 +0530

    add one line extra

--- firstproj.java
+++ firstproj.java
@@ -2,5 +2,6 @@ public class firstproj{
  public static void main(String[] args) {
   // TODO Auto-generated method stub
     System.out.println("Hello Agni");
+    System.out.println("hello");
  }
 }
