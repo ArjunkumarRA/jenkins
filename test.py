5a599d9c977009a72148308b5a964d2ef4bc700f
ArjunkumarRA <101347086+ArjunkumarRA@users.noreply.github.com>
2022-07-13 11:48:04 +0530

    test.py
    demo

---
+++ test.py
@@ -0,0 +1,18 @@
+import  pytest
+
+@pytest.fixture()
+def fac(n):
+    if n<0:
+        print("Enter a Postive Number")
+    elif (n==1 or n==0):
+        return 1
+    else:
+        return n * fac(n-1)
+    print("The factorial of the entered number : ", fac(n))
+n=5
+
+
+def test_num():
+    assert type(n) == int
+def test_size():
+    assert n!=5
